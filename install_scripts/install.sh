e2b9ad1e8d98debd5dc8d3881ee553fc9856457a
Yuta Nagamiya <y.nagamiya@gmail.com>
2021-03-21 23:05:31 +0900

    Update the installation script

--- install_scripts/install.sh
+++ install_scripts/install.sh
@@ -7,9 +7,9 @@ if (( $# != 1 )); then
   exit 1
 fi
 
+sed -i '/- name: Install myself to test myself/{n;n;d;}' .github/workflows/php.yml
+sed -i '/- name: Install myself to test myself/{n;d;}' .github/workflows/php.yml
 sed -i '/- name: Install myself to test myself/d' .github/workflows/php.yml
-sed -i '/  run: \.\/install\.sh ngmy\/library-template/{n;d;}' .github/workflows/php.yml
-sed -i '/  run: \.\/install\.sh ngmy\/library-template/d' .github/workflows/php.yml
 
 (
   ./install_scripts/replace-variables.sh "$1"
