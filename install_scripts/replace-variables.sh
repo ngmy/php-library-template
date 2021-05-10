c7ee624b0c59ee135228ceea7f858adfc2a35ff3
Yuta Nagamiya <y.nagamiya@gmail.com>
2021-05-11 01:11:09 +0900

    Fix

--- install_scripts/replace-variables.sh
+++ install_scripts/replace-variables.sh
@@ -17,7 +17,7 @@ readonly package_name_lower="${package_name,,}"
 readonly package_name_pascal="$(echo "${package_name}" | sed -r 's/(^|[_-]+)(.)/\U\2\E/g')"
 
 # `git grep -Il ''` means https://stackoverflow.com/questions/18973057/how-to-list-all-text-non-binary-files-in-a-git-repository/24350112#24350112
-git grep -Il '' | grep -v install.sh | xargs sed -i "s/{{ NGMY_COMPOSER_PACKAGE_NAME }}/${vendor_name_lower}\/${package_name_lower}/g"
-git grep -Il '' | grep -v install.sh | xargs sed -i "s/{{ NGMY_COMPOSER_AUTOLOAD_NAMESPACE_PREFIX }}/${vendor_name_pascal}\\\\\\\\${package_name_pascal}\\\\\\\\/g"
-git grep -Il '' | grep -v install.sh | xargs sed -i "s/{{ NGMY_LARADOCK_CONTAINER_PREFIX }}/laradock-${vendor_name_lower}-${package_name_lower}/g"
-git grep -Il '' | grep -v install.sh | xargs sed -i "s/{{ NGMY_PHP_NAMESPACE_PREFIX }}/${vendor_name_pascal}\\\\${package_name_pascal}/g"
+git grep -Il '' | grep -v replace-variables.sh | xargs sed -i "s/{{ NGMY_COMPOSER_PACKAGE_NAME }}/${vendor_name_lower}\/${package_name_lower}/g"
+git grep -Il '' | grep -v replace-variables.sh | xargs sed -i "s/{{ NGMY_COMPOSER_AUTOLOAD_NAMESPACE_PREFIX }}/${vendor_name_pascal}\\\\\\\\${package_name_pascal}\\\\\\\\/g"
+git grep -Il '' | grep -v replace-variables.sh | xargs sed -i "s/{{ NGMY_LARADOCK_CONTAINER_PREFIX }}/laradock-${vendor_name_lower}-${package_name_lower}/g"
+git grep -Il '' | grep -v replace-variables.sh | xargs sed -i "s/{{ NGMY_PHP_NAMESPACE_PREFIX }}/${vendor_name_pascal}\\\\${package_name_pascal}/g"
